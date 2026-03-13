78f05ae1f21df3bacde4dcf272ebe8c94ad3702a
root <root@Laptop-Antoine>
2026-03-13 09:43:15 +0100

    FIN DU 3 : A DEBUGGER

---
+++ 25-26-s2-tp-res-sr2/src/proto_tdd_v3_emetteur.c
@@ -0,0 +1,91 @@
+/*************************************************************
+* proto_tdd_v0 -  émetteur                                   *
+* TRANSFERT DE DONNEES  v0                                   *
+*                                                            *
+* Protocole sans contrôle de flux, sans reprise sur erreurs  *
+*                                                            *
+* Université de Toulouse / FSI / Dpt d'informatique          *
+**************************************************************/
+
+#include <stdio.h>
+#include "application.h"
+#include "couche_transport.h"
+#include "services_reseau.h"
+#include <stdlib.h>
+#define MAXTRANSMISSION 100
+
+/* =============================== */
+/* Programme principal - émetteur  */
+/* =============================== */
+int main(int argc, char* argv[])
+{
+    unsigned char message[MAX_INFO]; /* message de l'application */
+    int taille_msg;                  /* taille du message */
+    paquet_t pack;                  /* paquet utilisé par le protocole */
+    paquet_t tab_p[16];
+    int evenement; 
+    int borne_inf = 0, curseur=0, i=0;
+    int taille_fenetre;
+    int param = atoi(argv[1]);
+    if(param<16 && param>0) {
+        taille_fenetre=param;
+    } else {
+        taille_fenetre=4;
+    }
+
+    init_reseau(EMISSION);
+
+    printf("[TRP] Initialisation reseau : OK.\n");
+    printf("[TRP] Debut execution protocole transport.\n");
+
+    de_application(message, &taille_msg);
+
+   
+    /* tant que l'émetteur a des données à envoyer */
+    while ( taille_msg != 0  || borne_inf != curseur) {
+        if(dans_fenetre(borne_inf,curseur,taille_fenetre) && (taille_msg>0)) {
+            
+             
+             
+              /* construction paquet */
+              for (int i=0; i<taille_msg; i++) {
+                tab_p[curseur].info[i] = message[i];
+            }
+            tab_p[curseur].lg_info = taille_msg;
+            tab_p[curseur].type = DATA;
+            tab_p[curseur].num_seq = curseur;
+            tab_p[curseur].somme_ctrl = generer_somme_controle(tab_p[curseur]);
+            vers_reseau(&tab_p[curseur]);
+
+            if (curseur == borne_inf) {
+            depart_temporisateur(70);
+            }
+            curseur = (curseur + 1) % 16; // Inc
+        /* lecture de donnees provenant de la couche application */
+        de_application(message, &taille_msg);
+        
+        } else { // Go Back N
+            evenement = attendre();
+            if (evenement == PAQUET_RECU) {
+                de_reseau(&pack);
+                if (dans_fenetre(borne_inf,curseur,taille_fenetre) && verifier_somme_controle(pack)){
+                    borne_inf = (pack.num_seq + 1) % 16;
+                    arret_temporisateur();
+                    if (borne_inf != curseur) {
+                        depart_temporisateur(70);
+                    }
+                }
+            } else {
+                i = borne_inf;
+                depart_temporisateur(70);
+                do {
+                    vers_reseau(&tab_p[i]);
+                    curseur = (curseur + 1) % 16;
+                } while (i!=curseur);
+            }
+        }
+    }
+
+    printf("[TRP] Fin execution protocole transfert de donnees (TDD).\n");
+    return 0;
+}
